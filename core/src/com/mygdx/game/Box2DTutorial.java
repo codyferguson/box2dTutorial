8ab8cc5f381dcefa614f9ee15dc033fd30f9ab10
Cody Ferguson <cfsoftwareengineer14@gmail.com>
2019-01-22 18:51:09 -0800

    working on setting up the main screen

--- core/src/com/mygdx/game/Box2DTutorial.java
+++ core/src/com/mygdx/game/Box2DTutorial.java
@@ -6,17 +6,30 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.views.*;
 
 // ApplicationAdapter if you want to use your own screen management
 // aka the Orchestrator
 public class Box2DTutorial extends Game {
 	SpriteBatch batch;
 	Texture img;
-	
+	private LoadingScreen loadingScreen;
+	private PreferencesScreen preferencesScreen;
+	private MenuScreen menuScreen;
+	private MainScreen mainScreen;
+	private EndScreen endScreen;
+
+	public final static int MENU = 0;
+    public final static int PREFERENCES = 1;
+    public final static int APPLICATION = 2;
+    public final static int ENDGAME = 3;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	    //batch = new SpriteBatch();
+        loadingScreen = new LoadingScreen(this);
+		//img = new Texture("badlogic.jpg");
+        setScreen(loadingScreen);
 	}
 
 	@Override
@@ -33,4 +46,29 @@ public class Box2DTutorial extends Game {
 		batch.dispose();
 		img.dispose();
 	}
+
+	public void changeScreen(int screen){
+	    switch (screen){
+            case MENU:
+                if(menuScreen == null)
+                    menuScreen = new MenuScreen(this);
+                this.setScreen(menuScreen);
+                break;
+            case PREFERENCES:
+                if(preferencesScreen == null)
+                    preferencesScreen = new PreferencesScreen(this);
+                this.setScreen(preferencesScreen);
+                break;
+            case APPLICATION:
+                if(mainScreen == null)
+                    mainScreen = new MainScreen(this);
+                this.setScreen(mainScreen);
+                break;
+            case ENDGAME:
+                if(endScreen == null)
+                    endScreen = new EndScreen(this);
+                this.setScreen(endScreen);
+                break;
+        }
+    }
 }
---
+++ core/src/com/mygdx/game/views/MenuScreen.java
@@ -0,0 +1,72 @@
+package com.mygdx.game.views;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.mygdx.game.Box2DTutorial;
+
+import javax.xml.soap.Text;
+
+public class MenuScreen implements Screen {
+    private Stage stage;
+    private Box2DTutorial parent;
+
+    public MenuScreen(Box2DTutorial box2DTutorial){
+        parent = box2DTutorial;
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        // here?
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+        // Create a table that fills the screen. Everything else will go inside this table
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(true);
+        stage.addActor(table);
+
+        Skin skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+
+        TextButton newGame = new TextButton("New Game", skin);
+        TextButton preferences = new TextButton("Preferences", skin);
+        TextButton exit = new TextButton("Exit", skin);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
